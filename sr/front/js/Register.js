8333c726f64a4d78a26563bfed3ef279e344a1ce
Minhdev2004 <chinhtpc123@gmail.com>
2024-12-16 13:22:08 +0700

    final sign/in/up

---
+++ sr/front/js/Register.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const steps = document.querySelectorAll(".step");
+    const contents = document.querySelectorAll(".content");
+    const nextButton = document.getElementById("next-btn");
+    let currentStep = 0;
+
+   
+    function updateStep(stepIndex) {
+        
+        contents[currentStep].classList.remove("content-active");
+        steps[currentStep].classList.remove("step-active");
+
+        
+        currentStep = stepIndex;
+        contents[currentStep].classList.add("content-active");
+        steps[currentStep].classList.add("step-active");
+
+        
+        nextButton.innerText = currentStep === steps.length - 1 ? "Finish" : "Next";
+    }
+
+    
+    nextButton.addEventListener("click", () => {
+        if (currentStep < steps.length - 1) {
+            updateStep(currentStep + 1);
+            steps[currentStep].classList.add("step-completed");
+        } else {
+            alert("Account creation process completed!");
+        }
+    });
+
+    
+    steps.forEach((step, index) => {
+        step.addEventListener("click", () => {
+            if (index <= currentStep) {
+                updateStep(index);
+            }
+        });
+    });
+});
