1d5a5e5f702e12f5fc0364d76fe8fa742fff6fe7
Minhdev2004 <chinhtpc123@gmail.com>
2025-01-03 00:29:43 +0700

    Owner store

---
+++ sr/front/js/Owner_store.js
@@ -0,0 +1,8 @@
+document.getElementById('get-directions').addEventListener('click', function () {
+    document.getElementById('map-modal').style.display = 'flex';
+  });
+  
+  document.getElementById('close-modal').addEventListener('click', function () {
+    document.getElementById('map-modal').style.display = 'none';
+  });
+  
