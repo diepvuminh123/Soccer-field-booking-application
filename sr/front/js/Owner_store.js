8c9a990f58f13a610b79d7e5a0df130afac58f38
Minhdev2004 <chinhtpc123@gmail.com>
2025-01-03 16:33:47 +0700

    add Makepayment

--- sr/front/js/Owner_store.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('get-directions').addEventListener('click', function () {
-    document.getElementById('map-modal').style.display = 'flex';
-  });
-  
-  document.getElementById('close-modal').addEventListener('click', function () {
-    document.getElementById('map-modal').style.display = 'none';
-  });
-  
