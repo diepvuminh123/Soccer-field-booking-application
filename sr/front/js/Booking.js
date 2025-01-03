8a0f97bc1386c1e4ee37417f49d812af661fadbe
baolong <165645836+b1onk@users.noreply.github.com>
2025-01-03 19:47:44 +0700

    Add files via upload

--- sr/front/js/Booking.js
+++ sr/front/js/Booking.js
@@ -2,11 +2,17 @@ document.getElementById('bookingForm').addEventListener('submit', function(event
     event.preventDefault(); 
 
     // Lấy giá trị 
-   
     const fieldName = document.getElementById('field-name').value;
     const fieldLoc = document.getElementById('field-location').value;
     const startTime = document.getElementById('start-time').value;
     const endTime = document.getElementById('end-time').value;
     const date = document.getElementById('date').value;
 
-})
+    if (startTime >= endTime) {
+        alert('Thời gian bắt đầu phải sớm hơn thời gian kết thúc!');
+        return;
+    }
+
+    // Display success message
+    document.getElementById('message').textContent = 'Đặt sân thành công!';
+});
